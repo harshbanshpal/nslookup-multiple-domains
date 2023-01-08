fdc0cb5bd5a501828873c041508045c4314cf798
harshbanshpal <102986994+harshbanshpal@users.noreply.github.com>
2023-01-08 15:05:21 +0530

    Update nslookup.sh

--- nslookup.sh
+++ nslookup.sh
@@ -2,6 +2,9 @@
 
 # Read each line of the file
 while read line; do
+  # Remove "https://" and "http://" from the beginning of the line
+  domain=$(echo "$line" | sed -e 's/https:\/\///' -e 's/http:\/\///')
+
   # Perform the nslookup
-  nslookup "$line"
+  nslookup "$domain"
 done < file.txt
